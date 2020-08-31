33c93df9ef8e5101b3da9225c1186331da60a0a7
jinho6225 <jinho6225@hotmail.com>
2020-08-30 17:06:00 -0700

    largest smaller bst key

---
+++ Largest Smaller BST Key.js
@@ -0,0 +1,135 @@
+/*
+Largest Smaller BST Key
+Given a root of a Binary Search Tree (BST) and a number num, implement an efficient function findLargestSmallerKey that finds the largest key in the tree that is smaller than num. If such a number doesn’t exist, return -1. Assume that all keys in the tree are nonnegative.
+
+Analyze the time and space complexities of your solution.
+
+For example:
+
+For num = 17 and the binary search tree below:
+
+        20
+      /   \
+    9       25
+  /   \
+5      12
+    /    \ 
+  11      14
+
+Your function would return:
+
+14 since it’s the largest key in the tree that is still smaller than 17.
+
+Constraints:
+
+[time limit] 5000ms
+[input] Node rootNode
+[output] integer
+*/
+
+
+/*********************************************************
+ * CODE INSTRUCTIONS:                                    *
+ * 1) The method findLargestSmallerKey you're            *
+ *    asked to implement is located at line 26.          *
+ * 2) Use the helper code below to implement it.         *
+ * 3) In a nutshell, the helper code allows you to       *
+ *    to build a Binary Search Tree.                     *
+ * 4) Jump to line 71 to see an example for how the      *
+ *    helper code is used to test findLargestSmallerKey. *
+ *********************************************************/
+
+
+// Constructor to create a new Node
+function Node(key) {
+    this.key = key;
+    this.parent = null;
+    this.left = null;
+    this.right = null;
+  }
+  
+  // Constructor to create a new BST 
+  function BinarySearchTree() {
+    this.root = null;
+  }
+  
+  BinarySearchTree.prototype.findLargestSmallerKey = function(num) {
+    // your code goes here
+    let cur = this.root // 20
+    let smallest = null
+    while (cur) { // 20, 19
+      if (cur.key < num) {
+        smallest = cur.key // 14
+        cur = cur.right
+      } else {
+        cur = cur.left
+      }
+    }
+    if (smallest === null) return -1
+    return smallest  
+    /*
+    define empty array = []
+    traversal this tree and if it's lower than num
+    if yes,
+      array.push(num)
+    return Math.max(...array)
+    [9,5,12,11,14]
+    20, 9,5,12,11,14,25
+    */
+
+  
+  // Creates a new node by a key and inserts it to the BST
+  BinarySearchTree.prototype.insert = function(key) {
+    var root = this.root;
+  
+    // 1. If the tree is empty, create the root
+    if(!root) {
+        this.root = new Node(key);
+        return;
+    }
+  
+    // 2) Otherwise, create a node with the key
+    //    and traverse down the tree to find where to
+    //    to insert the new node
+    var currentNode = root;
+    var newNode = new Node(key);
+  
+    while(currentNode !== null) {
+        if(key < currentNode.key) {
+            if(!currentNode.left) {
+                currentNode.left = newNode;
+                newNode.parent = currentNode;
+                break;
+            } else {
+                currentNode = currentNode.left;
+            }
+        } else {
+            if(!currentNode.right) {
+                currentNode.right = newNode;
+                newNode.parent = currentNode;
+                break;
+            } else {
+                currentNode = currentNode.right;
+            }
+        }
+    }
+  }
+  
+  /*********************************************
+   * Driver program to test above function     *
+   *********************************************/
+  
+  // Create a Binary Search Tree
+  var bst = new BinarySearchTree();
+  bst.insert(20);
+  bst.insert(9);
+  bst.insert(25);
+  bst.insert(5);
+  bst.insert(12);
+  bst.insert(11);
+  bst.insert(14);
+  
+  var result = bst.findLargestSmallerKey(17);
+  
+  console.log("Largest smaller number is " + result);
+  
