067410fe0e80b8f0f39056c4a8d70c48a802f8dc
jinho6225 <jinho6225@hotmail.com>
2020-09-20 16:26:21 -0700

    another solution added

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -43,3 +43,20 @@ var rockPaperScissors = function () {
 		return list
 	}
 };
+
+const rockPaperScissors = roundCount => {
+	if (roundCount === 0) return []
+	const permutations = []
+	function playRound(plays) {
+		if (plays.length === roundCount) {
+			permutations.push(plays)
+			return
+		}
+		['r', 'p', 's'].forEach(play => {
+			playRound(plays + play)
+		})
+	}
+	playRound('')
+	return permutations
+}
+rockPaperScissors(3)
