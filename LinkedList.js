6cb43f440525be316136db1fe4e78efc36678514
jinho6225 <jinho6225@hotmail.com>
2020-11-15 19:31:55 -0800

    implement insert method

--- LinkedList.js
+++ LinkedList.js
@@ -11,6 +11,12 @@
 //     }
 //   }
 // };
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.next = null;
+    }
+}
 
 class LinkedList {
     constructor(value) {
@@ -23,10 +29,7 @@ class LinkedList {
     }
 
     append(value) {
-        const newNode = {
-            value: value,
-            next: null
-        }
+        const newNode = new Node(value)
         this.tail.next = newNode
         this.tail = newNode
         this.length++
@@ -34,10 +37,7 @@ class LinkedList {
     }
 
     prepend(value) {
-        const newNode = {
-            value: value,
-            next: null
-        }
+        const newNode = new Node(value)
         // let curHead = this.head;
         // this.head = newNode
         // this.head.next = curHead
@@ -46,11 +46,44 @@ class LinkedList {
         this.length++
         return this
     }
+    printList() {
+        const array = [];
+        let currentNode = this.head;
+        while (currentNode !== null) {
+            array.push(currentNode.value)
+            currentNode = currentNode.next
+        }
+        return array
+    }
+    insert(index, value){
+        //Code here
+        let node = new Node(value)
+        let cur = this.head
+        let prev = null
+        let i = 1
+        while (cur) {
+            if (index === i) {
+                prev.next = node
+                node.next = cur
+                break;
+            }
+            prev = cur
+            cur = cur.next
+            i++
+        }
+        return this.printList();
+    }
 }
-
 const myLinkedList = new LinkedList(10)
 myLinkedList.append(5)
 myLinkedList.append(16)
 myLinkedList.prepend(1)
+console.log(myLinkedList.printList())
+myLinkedList.insert(2, 99)
+console.log(myLinkedList.printList())
 
 console.log(myLinkedList)
+
+// 1 --> 10      5 --> 16
+//        \    / 
+//          99
