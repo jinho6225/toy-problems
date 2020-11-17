5089171f5123f4fc13da633f25078573ba42cf37
jinho6225 <jinho6225@hotmail.com>
2020-11-17 11:53:59 -0800

    better solution added O(n), O(1)

--- Time Planner.js
+++ Time Planner.js
@@ -21,4 +21,31 @@ function meetingPlanner(slotsA, slotsB, dur) {
   
   //Jin-ho
   //Sebastian
+
+//tiem: O(n)
+//space: O(1)
+  function meetingPlanner(slotsA, slotsB, dur) {
+    let po1 = 0, po2 = 0, result
+    while (po1 < slotsA.length || po2 < slotsB.length) {
+      if (slotsB[po2] === undefined) break;
+      if (slotsA[po1][1] > slotsB[po2][0]) {
+        result = helper(slotsA[po1], slotsB[po2], dur)
+        if (result) {
+          return [Math.max(slotsA[po1][0], slotsB[po2][0]), (Math.max(slotsA[po1][0], slotsB[po2][0]) + dur)]
+        } else {
+          po2++
+        }
+      } else {
+        po1++
+        po2 = 0
+      }
+    }
+    return []
+  }
+  
+  function helper(a, b, dur) {
+    return Math.max(a[0], b[0]) + dur <= Math.min(a[1], b[1])
+  }
+
+
   
