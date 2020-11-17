39858423de3d352b0636881878b6c44969f38800
jinho6225 <jinho6225@hotmail.com>
2020-11-16 18:27:55 -0800

    implementation Queue with linked list

---
+++ Queue Implementation.js
@@ -0,0 +1,52 @@
+class Node {
+    constructor(value) {
+      this.value = value;
+      this.next = null;
+    }
+  }
+  
+  class Queue {
+    constructor(){
+      this.first = null;
+      this.last = null;
+      this.length = 0;
+    }
+    peek() {
+        return this.first
+    }
+    enqueue(value){
+        let newNode = new Node(value)
+        if (this.length === 0) {
+            this.last = newNode
+            this.first = newNode
+        } else {
+            this.last.next = newNode;
+            this.last = newNode
+        }
+        this.length++
+        return this
+    }
+    dequeue(){
+        if (!this.first) return null
+        if (this.first === this.last) this.last = null
+        this.first = this.first.next
+        this.length--
+        return this
+    }
+    //isEmpty;
+  }
+  
+  const myQueue = new Queue();
+  myQueue.enqueue('Joy')
+  myQueue.enqueue('Matt')
+  myQueue.enqueue('Pavel')
+  myQueue.enqueue('Samir')
+
+  myQueue.dequeue()
+  console.log(myQueue.peek())
+
+  console.log(myQueue)
+  //Joy
+  //Matt
+  //Pavel
+  //Samir
