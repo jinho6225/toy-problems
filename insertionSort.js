2c4994e63d4e48dc36a9aee7473b11d57fc18cce
jinho6225 <jinho6225@hotmail.com>
2020-11-17 11:54:39 -0800

    another solution added

--- insertionSort.js
+++ insertionSort.js
@@ -48,3 +48,26 @@ function insertionSort(array) {
 
 insertionSort(numbers);
 console.log(numbers);
+
+
+let numbers = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0];
+function insertionSort(array) {
+	for (let i = 1; i < array.length; i++) { 
+		let j = i-1;
+		let k = i
+			while(j >= 0) {
+			  	if (array[k] < array[j]) {
+					let tmp = array[k]
+					array[k] = array[j]
+					array[j] = tmp	
+					j--
+					k--
+		  		} else {
+					break;;
+				} 
+	  	}
+  	}
+}
+
+insertionSort(numbers);
+console.log(numbers);
